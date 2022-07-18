0c677ed40f82c628e4bdd7a7b3de581c32d3c43a
SDen4 <asw325@yandex.ru>
2022-07-18 21:25:39 +0500

    httpRequest function

---
+++ src/store/main/httpRequest.ts
@@ -0,0 +1,8 @@
+import { API } from '../../utils/api';
+
+async function httpRequest() {
+  const response = await API.get('getInitData').then((res) => res.data);
+  return response;
+}
+
+export default httpRequest;
--- src/store/main/sagas/saga.ts
+++ src/store/main/sagas/saga.ts
@@ -2,18 +2,13 @@ import { put, takeEvery } from 'redux-saga/effects';
 
 import { getInitDataSaga, saveInitData } from '../duck';
 
-import { API } from '../../../utils/api';
+import httpRequest from '../httpRequest';
 
 import { initDataType } from '../types/types';
 
-async function getData() {
-  const response = await API.get('getInitData').then((res) => res.data);
-  return response;
-}
-
 function* getInitData() {
   try {
-    const initData: initDataType = yield getData();
+    const initData: initDataType = yield httpRequest();
     yield put(saveInitData(initData));
   } catch (error) {
     console.log(error);
