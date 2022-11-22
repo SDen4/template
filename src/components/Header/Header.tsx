13a041421d64bf7faeb371c406c06c5de14786e9
SDen4 <asw325@yandex.ru>
2022-11-22 20:57:21 +0500

    refactor: Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,8 +6,7 @@ export const Header: FC = () => {
   return (
     <header>
       <h1 className={styles.appHeader}>
-        Typescript - CSS - React - Redux - Redux-saga - Redux-toolkit - Axios -
-        Jest
+        Typescript - CSS - React - Redux-saga - Redux-toolkit - Axios - Jest
       </h1>
     </header>
   );
