3df26f37031ca597f7b3df8c518e6d9e5f4b53cf
SDen4 <asw325@yandex.ru>
2023-02-26 17:09:13 +0500

    refactor: httpRequest func

--- src/api/httpRequest.ts
+++ src/api/httpRequest.ts
@@ -9,15 +9,15 @@ export async function httpRequest(
   const allData = await fetch(
     `https://api.github.com/${path}`,
     params && postObject(params),
-  )
-    .then((res) => {
-      status = res.status;
+  ).then((res) => {
+    status = res.status;
 
-      return res.json();
-    })
-    .catch(() => {});
+    return res.json();
+  });
 
-  if (status !== 200) throw new Error('Request error!');
+  if (status !== 200) {
+    throw new Error('Request error!');
+  }
 
   return allData;
 }
